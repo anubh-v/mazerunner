8a38e04daaf3baef8f4dc4f2a7cdca72989cc416
anubh-v <->
2021-01-08 13:15:59 +0800

    Add runnable threads queue

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,6 +1,12 @@
 #include "thread.h"
+#include <queue>
+
+using namespace std;
 
 void interpret(char** program) {
+
+    // setup a queue of threads (empty for now)
+    queue<Thread> runnable_threads;
 
     // setup first thread
       // create empty stack for thread
