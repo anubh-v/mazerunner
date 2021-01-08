cc5d0dcf952279e95549c2882c94c9f577675167
anubh-v <anubhav@u.nus.edu>
2021-01-08 13:46:58 +0800

    Add interpreter loop

--- interpreter.cpp
+++ interpreter.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 void set_starting_pointer(Thread thread, char** program);
 
+void run_single_instruction(char** program, Thread thread);
+
 void interpret(char** program) {
 
     // setup a queue of threads (empty for now)
@@ -13,12 +15,27 @@ void interpret(char** program) {
     // setup first thread
     Thread root_thread;
     set_starting_pointer(root_thread, program);
+    queue.push(runnable_threads);
 
-    // interpret program according to instructions
+    while(true) {
+        // get thread to run
+        Thread current_thread = runnable_threads.pop();
 
+        // interpret program according to instructions
+        run_single_instruction(program, current_thread);
+
+        // suspend thread
+        runnable_threads.push(current_thread);
+
+    }
 }
 
 // set the initial program pointer for a program
 void set_starting_pointer(Thread thread, char** program) {
     
 }
+
+void run_single_instruction(char** program, Thread thread) {
+}
+
+
