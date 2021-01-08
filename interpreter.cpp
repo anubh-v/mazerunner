019bdd7d323c00db150ae35f10ab82952937f460
anubh-v <->
2021-01-08 13:13:48 +0800

    Add basic outline

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,3 +1,12 @@
 #include "thread.h"
 
-void interpret(char** program) { }
+void interpret(char** program) {
+
+    // setup first thread
+      // create empty stack for thread
+      // set current direction = right
+      // find starting point in the program
+    
+    // interpret program according to instructions
+
+}
