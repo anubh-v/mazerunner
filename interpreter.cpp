ba697bd6cf401c2f9326b9e3e1ee946582015ece
anubh-v <->
2021-01-08 13:27:45 +0800

    Add function to set starting pointer

--- interpreter.cpp
+++ interpreter.cpp
@@ -3,16 +3,22 @@
 
 using namespace std;
 
+void set_starting_pointer(Thread thread, char** program);
+
 void interpret(char** program) {
 
     // setup a queue of threads (empty for now)
     queue<Thread> runnable_threads;
 
     // setup first thread
-      // create empty stack for thread
-      // set current direction = right
-      // find starting point in the program
-    
+    Thread root_thread;
+    set_starting_pointer(root_thread, program);
+
     // interpret program according to instructions
 
+}
+
+// set the initial program pointer for a program
+void set_starting_pointer(Thread thread, char** program) {
+    
 }
