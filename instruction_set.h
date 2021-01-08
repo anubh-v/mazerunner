872a54b61264df9a4911e8146021528caf98cf69
anubh-v <anubhav@u.nus.edu>
2021-01-08 19:46:43 +0800

    Add function to deduce if a char is an instruction

---
+++ instruction_set.h
@@ -0,0 +1,48 @@
+
+
+bool is_instruction(char possible_inst) {
+
+   switch(possible_inst) {
+       case '_':
+       case '0':
+       case '1':
+       case '2':
+       case '3':
+       case '4':
+       case '5':
+       case '6':
+       case '7':
+       case '8':
+       case '9':
+       case ')':
+       case '(':
+       case '+':
+       case '-':
+       case '*':
+       case '/':
+       case '%':
+       case '`':
+       case '&':
+       case '|':
+       case '$':
+       case '~':
+       case ':':
+       case ';':
+       case '}':
+       case '{':
+       case '=':
+       case '#':
+       case ',':
+       case '?':
+       case '.':
+       case '!':
+       case '<':
+       case '>':
+       case '^':
+       case 'v':
+           return true;
+       default:
+           return false;
+   }
+
+}
--- interpreter.cpp
+++ interpreter.cpp
@@ -1,16 +1,18 @@
 #include "thread.h"
 #include <queue>
 #include "instruction_dispatcher.h"
+#include "instruction_set.h"
+#include "src/parser.h"
 
 using namespace std;
 
-void set_starting_pointer(Thread thread, char** program);
+void set_starting_pointer(Thread thread, Program program);
 
-void run_single_instruction(char** program, Thread thread);
+void run_single_instruction(Program program, Thread thread);
 
-void update_direction(char** program, Thread thread);
+void update_direction(Program program, Thread thread);
 
-void interpret(char** program) {
+void interpret(Program program) {
 
     // setup a queue of threads (empty for now)
     queue<Thread> runnable_threads;
@@ -35,11 +37,31 @@ void interpret(char** program) {
 }
 
 // set the initial program pointer for a program
-void set_starting_pointer(Thread thread, char** program) {
-    
+void set_starting_pointer(Thread thread, Program program) {
+    // find the first character that represents an instruction
+    int num_rows = program.nr;
+    int num_cols = num_rows; // assume same number of cols and rows
+    char** grid = program.grid;
+
+    for(int i = 0; i < num_rows; i++) {
+        char* row = grid[i];
+        for (int j = 0; j < num_cols; j++) {
+            char current_char = row[j];
+            if (is_instruction(current_char)) {
+                thread.program_pointer[0] = j; // set x coord
+                thread.program_pointer[1] = i; // set y coord
+                return;
+            }
+        }
+    }
+
+
+   // if control reaches here, we have an error:
+   // the program has no starting point. How to handle?
+
 }
 
-void run_single_instruction(char** program, Thread thread) {
+void run_single_instruction(Program program, Thread thread) {
     thread.move_to_next_inst();
 
     char instruction; // get current instruction
